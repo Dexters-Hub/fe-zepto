9b75c8c23243d498edab9d1811dba80adcab33a1
Alwin <alwinjohn231@gmail.com>
2024-01-16 16:41:28 +0530

    chore: dummyData created

---
+++ src/constants/dummyData.ts
@@ -0,0 +1,126 @@
+// constants/dummyData.ts
+const dummyData = [
+  {
+    id: 1,
+    name: {
+      title: 'Mr',
+      first: 'John',
+      last: 'Doe',
+    },
+    email: 'john.doe@example.com',
+    picture: {
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
+    },
+  },
+  {
+    id: 2,
+    name: {
+      title: 'Mrs',
+      first: 'Jane',
+      last: 'Smith',
+    },
+    email: 'jane.smith@example.com',
+    picture: {
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
+    },
+  },
+  {
+    id: 3,
+    name: {
+      title: 'Mr',
+      first: 'Robert',
+      last: 'Johnson',
+    },
+    email: 'robert.johnson@example.com',
+    picture: {
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/3.jpg',
+    },
+  },
+  {
+    id: 4,
+    name: {
+      title: 'Ms',
+      first: 'Amanda',
+      last: 'Clark',
+    },
+    email: 'amanda.clark@example.com',
+    picture: {
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/4.jpg',
+    },
+  },
+  {
+    id: 5,
+    name: {
+      title: 'Mr',
+      first: 'Daniel',
+      last: 'Williams',
+    },
+    email: 'daniel.williams@example.com',
+    picture: {
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/5.jpg',
+    },
+  },
+  {
+    id: 6,
+    name: {
+      title: 'Miss',
+      first: 'Emily',
+      last: 'Davis',
+    },
+    email: 'emily.davis@example.com',
+    picture: {
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/6.jpg',
+    },
+  },
+  {
+    id: 7,
+    name: {
+      title: 'Mr',
+      first: 'Christopher',
+      last: 'Moore',
+    },
+    email: 'christopher.moore@example.com',
+    picture: {
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/7.jpg',
+    },
+  },
+  {
+    id: 8,
+    name: {
+      title: 'Mrs',
+      first: 'Olivia',
+      last: 'Baker',
+    },
+    email: 'olivia.baker@example.com',
+    picture: {
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/8.jpg',
+    },
+  },
+  {
+    id: 9,
+    name: {
+      title: 'Mr',
+      first: 'Matthew',
+      last: 'Young',
+    },
+    email: 'matthew.young@example.com',
+    picture: {
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/9.jpg',
+    },
+  },
+  {
+    id: 10,
+    name: {
+      title: 'Miss',
+      first: 'Sophia',
+      last: 'Ward',
+    },
+    email: 'sophia.ward@example.com',
+    picture: {
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/10.jpg',
+    },
+  },
+
+];
+
+export default dummyData;
